11f7f63ebb6d7767343a3ad84e9255cc4c1fbf4d
Anatolii <kakofonn@gmail.com>
2016-11-26 14:31:04 +0200

    Moved fragment to another package

--- app/src/main/java/com/github/energion/energionandroid/manual/ManualFragment.java
+++ app/src/main/java/com/github/energion/energionandroid/manual/ManualFragment.java
@@ -1,4 +1,4 @@
-package com.github.energion.energionandroid;
+package com.github.energion.energionandroid.manual;
 
 import android.content.Context;
 import android.net.Uri;
@@ -8,18 +8,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.github.energion.energionandroid.R;
 import com.github.mikephil.charting.charts.LineChart;
 
 
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link Manual.OnFragmentInteractionListener} interface
+ * {@link ManualFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link Manual#newInstance} factory method to
+ * Use the {@link ManualFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Manual extends Fragment {
+public class ManualFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -31,7 +32,7 @@ public class Manual extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
-    public Manual() {
+    public ManualFragment() {
         // Required empty public constructor
     }
 
@@ -41,11 +42,11 @@ public class Manual extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment Manual.
+     * @return A new instance of fragment ManualFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static Manual newInstance(String param1, String param2) {
-        Manual fragment = new Manual();
+    public static ManualFragment newInstance(String param1, String param2) {
+        ManualFragment fragment = new ManualFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
