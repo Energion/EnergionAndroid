d606e5ecaaf7f6237bdff3750afb19e1bc63387e
alunicus <alunicus@gmail.com>
2016-11-26 15:09:28 +0200

    Removed unused view

--- app/src/main/java/com/github/energion/energionandroid/MainActivity.java
+++ app/src/main/java/com/github/energion/energionandroid/MainActivity.java
@@ -2,8 +2,6 @@ package com.github.energion.energionandroid;
 
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -13,7 +11,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 import com.github.energion.energionandroid.manual.ManualFragment;
 import com.github.energion.energionandroid.recommendation.RecommendationFragment;
@@ -38,15 +35,6 @@ public class MainActivity extends AppCompatActivity implements ManualFragment.On
 
     TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
     tabLayout.setupWithViewPager(pager);
-
-    FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-    fab.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View view) {
-        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-            .setAction("Action", null).show();
-      }
-    });
   }
 
   @Override
