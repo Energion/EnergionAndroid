25051372c47f8ce0b36b490a7433d82523288b38
Anatolii <kakofonn@gmail.com>
2016-11-26 18:57:55 +0200

    Manual fragment added graphs and mock data

--- app/src/main/java/com/github/energion/energionandroid/manual/ManualFragment.java
+++ app/src/main/java/com/github/energion/energionandroid/manual/ManualFragment.java
@@ -1,15 +1,36 @@
 package com.github.energion.energionandroid.manual;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.github.energion.energionandroid.R;
+import com.github.energion.energionandroid.model.Day;
+import com.github.energion.energionandroid.model.Hour;
+import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.listener.ChartTouchListener;
+import com.github.mikephil.charting.listener.OnChartGestureListener;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 
 /**
@@ -30,7 +51,13 @@ public class ManualFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
+    private List<Day> daysList = new ArrayList<>();
+    private BarChart barChart;
+    private BarData barData;
+
     private OnFragmentInteractionListener mListener;
+    private OnChartValueSelectedListener chartSelectionListener;
+    private TextView priceText;
 
     public ManualFragment() {
         // Required empty public constructor
@@ -61,7 +88,28 @@ public class ManualFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
-        LineChart chart = (LineChart) getView().findViewById(R.id.chart);
+        chartSelectionListener = new OnChartValueSelectedListener(){
+            @Override
+            public void onValueSelected(Entry e, Highlight h) {
+//                priceText.setText(String.valueOf(((Hour)e.getData()).getPrice()));
+                priceText.setText(String.valueOf(e.getY()) + " " + getResources().getString(R.string.selected_price_currency));
+            }
+
+            @Override
+            public void onNothingSelected() {
+
+            }
+        };
+//        daysList.add(new Day());
+//        daysList.get(0).setDate(new Date().toString());
+//        List<Hour> hours = new ArrayList<>();
+//        for (int i = 0; i < 35; i++) {
+//            Hour h = new Hour();
+//            h.setHour(String.valueOf(i));
+//            h.setPrice((float)i);
+//            hours.add(h);
+//        }
+//        daysList.get(0).setHours(hours);
 
     }
 
@@ -69,7 +117,54 @@ public class ManualFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_manual, container, false);
+        View view = inflater.inflate(R.layout.fragment_manual, container, false);
+
+        priceText = (TextView) view.findViewById(R.id.selected_price);
+        List<BarEntry> entries = new ArrayList<>();
+        float start = 1f;
+        for (Day day : daysList) {
+            for (Hour hour : day.getHours()) {
+//                entries.add(new BarEntry(hour.getPrice(), start, hour));
+                start++;
+            }
+        }
+        for (int i = 1; i < 50; i++) {
+
+            entries.add(new BarEntry(i, i));
+        }
+
+        BarDataSet barDataSet = new BarDataSet(entries, "BarDataSet");
+        int[] colors = new int[barDataSet.getEntryCount()];
+        for (int i = 0; i < colors.length; i++){
+            float selectedPrice = barDataSet.getEntryForIndex(i).getY() - getMinimumPrice();
+            float priceRange = (getMaximumPrice() - getMinimumPrice()) / 3;
+            Log.d("FragmentColors: ", "Selected price: " + selectedPrice + ", priceRange: " + priceRange);
+            if (selectedPrice < priceRange) {
+                colors[i] = Color.parseColor("#F44242");
+            } else if (selectedPrice > (priceRange * 2)) {
+                colors[i] = Color.parseColor("#45F442");
+            } else {
+                colors[i] = Color.parseColor("#F4DC42");
+            }
+        }
+        barDataSet.setColors(colors);
+
+        barData = new BarData(barDataSet);
+        barChart = (BarChart) view.findViewById(R.id.chart);
+        barChart.setFitBars(true);
+        barChart.setData(barData);
+        barChart.setVisibleXRangeMinimum(0f);
+        barChart.setVisibleYRangeMinimum(0f, YAxis.AxisDependency.RIGHT);
+        barChart.getAxisLeft().setDrawGridLines(false);
+        barChart.getAxisRight().setDrawGridLines(false);
+        barChart.getXAxis().setDrawGridLines(false);
+        barChart.getXAxis().setDrawAxisLine(false);
+        barChart.getAxisLeft().setDrawAxisLine(false);
+        barChart.getAxisRight().setDrawAxisLine(false);
+        barChart.setOnChartValueSelectedListener(chartSelectionListener);
+        barChart.invalidate();
+
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -77,6 +172,30 @@ public class ManualFragment extends Fragment {
         if (mListener != null) {
             mListener.onFragmentInteraction(uri);
         }
+    }
+
+    private float getMinimumPrice() {
+        return getPrice(false);
+    }
+
+    private float getMaximumPrice() {
+        return getPrice(true);
+    }
+
+    private float getPrice(boolean maximum) {
+        float result = -1f;
+        for (Day d : daysList) {
+            for (Hour h : d.getHours()) {
+                if (result == -1f) {
+                    result = h.getPrice();
+                } else if (maximum && h.getPrice() > result) {
+                    result = h.getPrice();
+                } else if (!maximum && h.getPrice() < result) {
+                    result = h.getPrice();
+                }
+            }
+        }
+        return result;
     }
 
     @Override
