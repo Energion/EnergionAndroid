634a43d4beed424809c2ef056d80bef9ea9b9d2a
alunicus <alunicus@gmail.com>
2016-11-27 05:47:41 +0200

    Added toasts on detail click

--- app/src/main/java/com/github/energion/energionandroid/details/DetailsActivity.java
+++ app/src/main/java/com/github/energion/energionandroid/details/DetailsActivity.java
@@ -2,7 +2,10 @@ package com.github.energion.energionandroid.details;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.github.energion.energionandroid.R;
 
@@ -20,12 +23,15 @@ public class DetailsActivity extends AppCompatActivity {
     String[] medium = getIntent().getStringExtra(MEDIUM).split(",");
     String[] expensive = getIntent().getStringExtra(EXPENSIVE).split(",");
 
+    LinearLayout cheapContainer = (LinearLayout) findViewById(R.id.detail_cheapest);
     TextView cheapPrice = (TextView) findViewById(R.id.detail_cheapest_price);
     TextView cheapTime = (TextView) findViewById(R.id.detail_cheapest_time);
 
+    LinearLayout mediumContainer = (LinearLayout) findViewById(R.id.detail_medium);
     TextView mediumPrice = (TextView) findViewById(R.id.detail_medium_price);
     TextView mediumTime = (TextView) findViewById(R.id.detail_medium_time);
 
+    LinearLayout expensiveContainer = (LinearLayout) findViewById(R.id.detail_expensive);
     TextView expensivePrice = (TextView) findViewById(R.id.detail_expensive_price);
     TextView expensiveTime = (TextView) findViewById(R.id.detail_expensive_time);
 
@@ -37,6 +43,27 @@ public class DetailsActivity extends AppCompatActivity {
 
     expensiveTime.setText(getHourRange(Integer.parseInt(expensive[0])));
     expensivePrice.setText(expensive[1] + "€/\nMWh");
+
+    cheapContainer.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Toast.makeText(DetailsActivity.this, "Notification for cheapest was added", Toast.LENGTH_SHORT).show();
+      }
+    });
+
+    mediumContainer.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Toast.makeText(DetailsActivity.this, "Notification for medium was added", Toast.LENGTH_SHORT).show();
+      }
+    });
+
+    expensiveContainer.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Toast.makeText(DetailsActivity.this, "Notification for expensive was added", Toast.LENGTH_SHORT).show();
+      }
+    });
   }
 
   private String getHourRange(int hour){
